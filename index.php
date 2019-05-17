9ee1c019e97507f97088c0a155275f413d2050d1
MrHuggies <thomasaghughes@gmail.com>
2019-05-17 08:22:30 +0100

    animation on welcome page, about page with sliders, jumbotron, navbar, colours

--- index.php
+++ index.php
@@ -8,17 +8,18 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <link href="https://fonts.googleapis.com/css?family=La+Belle+Aurore|Open+Sans" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   <link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="styles.css" type="text/css">
+
+  <link rel="stylesheet" type="text/css" href="styles.css" media="screen" />
+
+
 </head>
 
 <body>
-
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light  scrolling-navbar">
     <a class="navbar-brand" href="#">Swaft Surveying</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -58,11 +59,8 @@
             </div>
             <div class="modal-body">
               <form id="contact-form" method="post" action="contact.php" role="form">
-
                 <div class="messages"></div>
-
                 <div class="controls">
-
                   <div class="row">
                     <div class="col-md-6">
                       <div class="form-group">
@@ -119,45 +117,101 @@
                         <strong>*</strong> These fields are required.
                       </div>
                     </div>
-                  </div>
-
+                  </div><!--controls-->
                 </form>
-              </div>
+              </div><!--modal-body-->
               <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                 <button type="button" class="btn btn-primary">Save changes</button>
-              </div>
+              </div><!--modal-footer-->
+            </div><!--modal-content-->
+          </div><!--modal-dialog-->
+        </div><!--modal/fade-->
+      </div><!--navbar/collapse-->
+    </nav><!--nav-->
+
+
+
+    <div class="view" style="background-image: url('http://localhost/EdsWebsite/images/architect-architecture-build-1109541.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+      <!-- Mask & flexbox options-->
+      <div class="mask rgba-gradient ">
+        <!-- Content -->
+        <div class="container intro">
+          <!--Grid row-->
+          <div class="row">
+            <!--Grid column-->
+            <div class="col-md-6 intro-text">
+              <h1 class="h1-responsive font-weight-bold mt-sm-5">Swaft <span style="color:#2ecc71; font-weight:800;">Surveying</span> </h1>
+              <hr class="hr-light">
+              <h6 class="typewriter">Final Account Reconciliation</h6>
+              <h6 class="typewriter2">Takeoffs & Tenders</h6>
+              <button type="button" class="btn btn-primary btn-1 orange-color-bg">Find Out More</button>
+            </div> <!--intro-text-->
+          </div><!--row-->
+        </div><!--container-fluid-->
+      </div><!--mask/rgba-garadient-->
+    </div><!--view-->
+
+    <div class="container" id="container2">
+      <h2>About Us</h2>
+      <div class="row">
+        <div class="about container about-text col-md-6">
+          <div id="about-bold-text"><h3>In today′s world, the importance of a well-executed web presence cannot be underestimated</h3>
+          </div>
+          <br><br>
+          <div id="about-light-text"><p>Our company’s history starts 20 years ago when we’ve decided to make the world a better place for those, who are the owners or smaller business! By now we’ve helped hundreds of top-flight companies and thousands of SMBs to call the right decisions on internal management, M&As, etc.</p>
+          </div>
+        </div><!--about/container-->
+
+
+        <div class="col-md-6 prog-bar container about">
+          <h3>Happy Customers</h3>
+          <div class="progress">
+            <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%">
+              big things
             </div>
           </div>
-        </div>
-      </div>
-    </nav>
+          <br>
+          <h3>Average Amount Saved</h3>
+          <div class="progress">
+            <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%">
+              <  Over 1000 people!<span class="align-right">hello</span>
+            </div>
+          </div>
+          <br>
+          <h3>Years Industry Experience</h3>
+          <div class="progress">
+            <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%">
+              <span> Over 10060 people!</span>
+            </div>
+          </div>
+        </div><!--prog-bar-->
+      </div><!--row-->
+    </div><!--container2-->
 
 
 
-  <section class="bgimage">
-    <div class="container-fluid">
+
+
+    <div class="view" id="container3" style="background-image: url('http://localhost/EdsWebsite/images/antique-architecture-backdrop-168442.jpg'); background-repeat: no-repeat; background-size: cover; background-attachment: fixed;" >
+        <div class="col-md-12 jumbotron no-bg" style="background-color:rgba(0, 0, 0, 0)" >
+          <div class="consultation text-center">
+            <h2>Every 1st consultation <br>
+              <span style="font-weight: 800">with us is free</span></h2>
+              <button type="button" class="btn btn-primary contact-button">Get In Touch</button>
+          </div>
+
+        </div><!--col-md-12-->
+
+    </div><!--container/container-3-->
+
+    <div class="container hello">
       <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-          <h5>Hello, world! Full width Hero-unit header</h5>
-          <p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-          <p><a href="#" class="btn btn-primary btn-large">Learn more »</a></p>
-        </div>
-      </div>
-    </div>
-  </section>
-
-    <div class="page-one container">
-      <div class="row" id="row-1">
         <div class="col-md-12">
-          <h1>Hello world ed is fuck tard</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <img src="C:\MAMP\htdocs\EdsWebsite\images\architect-architecture-build-1109541.jpg" alt="">
+
         </div>
 
       </div>
 
     </div>
-
-
   </body>
